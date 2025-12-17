8906b5e2cf9c3ba9b9b630d1cdc5de2c74e26149
Valentina Herrera <vahegiraldo@gmail.com>
2025-12-16 20:51:12 -0500

    useState y funcion data fetching. Terminados.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type FetchResult<T> = {
   data: T | null;
@@ -9,13 +9,38 @@ type FetchResult<T> = {
 const useFetch = <T,>(url: string): FetchResult<T> => {
 
   //TODO: 📌 Completar los useState e implementar la funcion de data fetching para que utilice la url que se pasa por parametro
-  const [data,] = useState<T | null>(null);
-  const [loading,] = useState(true);
-  const [error,] = useState<string | null>(null);
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await fetch(url);
+        console.log(response);
+        if(!response.ok) {
+          throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (err: unknown) {
+        if (err instanceof Error && err.name !== 'AbortError') {
+          setError(err.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
 
-
-  return { data, loading, error };
-};
+  return {
+    data: data,
+    loading,
+    error
+  };
+};  
 
 export default useFetch;
