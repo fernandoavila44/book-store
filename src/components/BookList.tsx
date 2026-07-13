36887113b846a79e8a0393ca0acd30cd410969e1
Claudia <claucdcc@gmail.com>
2026-07-13 16:13:51 -0500

    Conectar boton Anadir al carrito con dispatch ADD_ITEM en BookList

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import type { Book } from '../types/book';
+import { useCart } from '../context/CartContext';
 
 const BookList = () => {
   // Usar useFetch para obtener libros de la API
   const { data: books, loading, error } = useFetch<Book[]>('http://localhost:3001/books');
+
+  const { dispatch } = useCart();
 
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -18,7 +21,7 @@ const BookList = () => {
           </Link>
           <p>${book.price.toFixed(2)}</p>
           <button
-            onClick={() => { }}
+            onClick={() => dispatch({ type: 'ADD_ITEM', payload: book })}
             aria-label={`Añadir ${book.title} al carrito`}
           >
             Añadir al carrito
