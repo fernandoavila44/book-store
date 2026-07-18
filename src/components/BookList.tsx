e106df2cf688bf9ed0c4b535e4794baf764f5353
Beto <albertomolinab@hotmail.com>
2026-07-18 16:32:44 -0500

    edit BookList

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import type { Book } from '../types/book';
+import { useCart } from '../context/CartContext';
 
 const BookList = () => {
   // Usar useFetch para obtener libros de la API
   const { data: books, loading, error } = useFetch<Book[]>('http://localhost:3001/books');
+  const { dispatch } = useCart();
+
+  const addToCart = (book: Book) => {
+    dispatch({ type: 'ADD_ITEM', payload: book });
+  };
 
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error: {error}</p>;
@@ -18,7 +24,7 @@ const BookList = () => {
           </Link>
           <p>${book.price.toFixed(2)}</p>
           <button
-            onClick={() => { }}
+            onClick={() => addToCart(book)}
             aria-label={`Añadir ${book.title} al carrito`}
           >
             Añadir al carrito
