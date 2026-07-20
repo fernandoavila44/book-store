1e9ea2abe938524f8d100fbd09aba875fc97eb14
Víctor Duran <vicduran3d@hotmail.com>
2026-07-19 22:26:47 -0500

    cartContext usefetch Ok

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -5,6 +5,45 @@ const CartContext = createContext<CartContextType | undefined>(undefined);
 
 //TODO: 📌 Implementar el reducer con acciones ADD_ITEM, REMOVE_ITEM y CLEAR para limpiar el carrito completamente
 const cartReducer = (state: CartState, action: CartAction): CartState => {
+  switch (action.type) {  
+    case 'ADD_ITEM' : {
+      const existingItem = state.items.find(item => item.id === action.payload.id);
+      return {
+        ...state,
+        items : existingItem
+          ? state.items.map( item => item.id === action.payload.id 
+            ? {...item, quantity: item.quantity + 1}
+            : item
+          )
+          : [...state.items, {...action.payload, quantity: 1} ],
+          total: state.total + action.payload.price,
+      };
+
+    }
+    case "REMOVE_ITEM":{
+      const itemRemove = state.items.find(item => item.id === action.payload);
+      if(!itemRemove) return state;
+      return {
+        ...state,
+        items : itemRemove.quantity > 1 
+          ? state.items.map( item => 
+            item.id === action.payload
+              ? {...item, quantity: item.quantity -1}
+              : item  
+          )
+          : state.items.filter(item => item.id !== action.payload),
+            total: state.total - itemRemove.price,
+      };
+    }
+    case "CLEAR_CART": {
+      return {
+        items: [],
+        total: 0
+      }
+    }
+    default:
+      return state;
+
 };
 
 export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type FetchResult<T> = {
   data: T | null;
@@ -9,11 +9,40 @@ type FetchResult<T> = {
 const useFetch = <T,>(url: string): FetchResult<T> => {
 
   //TODO: 📌 Completar los useState e implementar la funcion de data fetching para que utilice la url que se pasa por parametro
-  const [data,] = useState<T | null>(null);
-  const [loading,] = useState(true);
-  const [error,] = useState<string | null>(null);
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+   console.log("Consultando datos del backend")
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await fetch(url);
+        console.log(response)
+        if (!response.ok) {
+          throw new Error("Error en la consulta al api")
+        }
+        const data = await response.json();
+        console.log(data)
+        setData(data);
+      } catch (error: unknown) {
+        console.log(error)
+        if (error instanceof Error) {
+          setError(error.message)
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
 
+    return () => {
+      console.log("Se desmonto el componente")
+    }
+  }, [url])
+  
 
   return { data, loading, error };
 };
